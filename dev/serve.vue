c3a334902ca3a270487232aabba0f60f4b58ab04
Artem Polikarpov <artemiypolikarpov@gmail.com>
2022-05-06 10:44:27 +0300

    Init

---
+++ dev/serve.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'ServeDev',
+});
+</script>
+
+<template>
+  <div id="app">
+    <BrInput />
+  </div>
+</template>
